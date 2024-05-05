9175352df8b9f75e0a97e60f6b4b3c4c56c05d8a
Saurabh Prakash <112266991+SaurabhPrakash2020@users.noreply.github.com>
2024-05-05 16:18:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={1000}
+              height={240}
               priority
             />
           </a>
